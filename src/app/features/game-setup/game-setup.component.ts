2b9573746275e23cda7773198e305bdd3f6861f2
Joshua Fiebak <jmfiebak@gmail.com>
2026-02-01 15:56:56 +0100

    add reactive form / add categories data

--- src/app/features/game-setup/game-setup.component.ts
+++ src/app/features/game-setup/game-setup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Category } from 'src/app/core';
+import { CATEGORIES } from 'src/app/core/data/categories.data';
 import { GameSize } from 'src/app/core/models/game-config.model';
 
 
@@ -10,57 +12,47 @@ import { GameSize } from 'src/app/core/models/game-config.model';
   styleUrls: ['./game-setup.component.scss']
 })
 export class GameSetupComponent {
+  availableCategories: Category[] = CATEGORIES;
   gameForm = new FormGroup({
     players: new FormArray([
       new FormControl('', Validators.required),
       new FormControl('', Validators.required),
-
     ]),
     gameSize: new FormControl<GameSize>('medium', Validators.required),
-
+    categories: new FormControl<Category[]>([], Validators.required),
   });
-  players: string[] = ['', ''];
-  questionsPerPair: number = 5;
-  numberOfRounds: number = 3;
-
-  questionsOptions = [5, 6, 7, 8, 9, 10];
-  roundsOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor(private router: Router) { }
 
+  get players(): FormArray {
+    return this.gameForm.get('players') as FormArray;
+  }
+
   addPlayer() {
-    this.players.push('');
+    this.players.push(new FormControl('', Validators.required));
   }
 
   removePlayer(index: number) {
     if (this.players.length > 2) {
-      this.players.splice(index, 1);
+      this.players.removeAt(index);
     }
+  }
+
+  startGame() {
+    if (this.gameForm.invalid) {
+      console.log((this.gameForm));
+
+      return
+    }
+
+    const { players, gameSize } = this.gameForm.value;
+    // → GameService aufrufen
+    return this.router.navigate(['/game-board']);
   }
 
   trackByIndex(index: number): number {
     return index;
   }
 
-  isValid(): boolean {
-    const validPlayers = this.players.filter(p => p.trim().length > 0);
-    return validPlayers.length >= 2;
-  }
 
-  startGame() {
-    if (!this.isValid()) {
-      return;
-    }
-
-    const validPlayers = this.players.filter(p => p.trim().length > 0);
-
-    const gameConfig = {
-      players: validPlayers,
-      questionsPerPair: this.questionsPerPair,
-      numberOfRounds: this.numberOfRounds
-    };
-
-    console.log('Game Config:', gameConfig);
-    this.router.navigate(['/game-board']);
-  }
 }
