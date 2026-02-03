df66de16c2100591bc03b90e356fd5968493c2e8
Joshua Fiebak <jmfiebak@gmail.com>
2026-02-03 18:47:36 +0100

    capacitor init
    - add ios and android platforms

---
+++ android/app/src/main/java/de/jmfiebak/back2back/MainActivity.java
@@ -0,0 +1,5 @@
+package de.jmfiebak.back2back;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.jmfiebak.back2back',
+  appName: 'Back2Back',
+  webDir: 'dist/back2back',
+};
+
+export default config;
